7a20e5f9d5c029ad4a0e3ce38a7cd99796edca61
grvnagpal <43705982+grvnagpal@users.noreply.github.com>
2018-10-01 01:06:34 +0530

    chrome

--- src/test/java/com/weather/e2e/OpenWeatherMapValidCityTest.java
+++ src/test/java/com/weather/e2e/OpenWeatherMapValidCityTest.java
@@ -21,7 +21,7 @@ public class OpenWeatherMapValidCityTest {
 	@BeforeMethod
 	public void setUp() throws Exception {
 		BasicConfigurator.configure();
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chrome/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		driver = new ChromeDriver();
 	}
 
